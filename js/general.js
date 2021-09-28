9669dc8324458285c2878e6e29b4b61becf3349e
kroloburet <kroloburet@gmail.com>
2021-09-28 13:38:36 +0300

    Удалить отложенную загрузку: "css/general.css", "Tagra_UI/style.css", "fontawesome"

--- js/general.js
+++ js/general.js
@@ -7,23 +7,6 @@
     /**
      * Отложенная загрузка ресурсов
      */
-    //////////// CSS
-    const head = document.getElementsByTagName('head')[0];
-    // главная таблица стилей
-    const gc = document.createElement('link');
-    gc.rel = 'stylesheet';
-    gc.href = 'css/general.css';
-    head.insertBefore(gc, head.firstChild);
-    // Tagra_UI
-    const tuiCss = document.createElement('link');
-    tuiCss.rel = 'stylesheet';
-    tuiCss.href = 'https://rawgit.com/kroloburet/TAGRA_CMS/master/Tagra_UI/style.css';
-    head.insertBefore(tuiCss, head.firstChild);
-    //////////// JS
-    // fontawesome
-    const fa = document.createElement('script');
-    fa.src = 'https://kit.fontawesome.com/bacee63d78.js'
-    head.insertBefore(fa, head.firstChild);
     // addThis
     setTimeout(() => {
         const at = document.createElement('script');
